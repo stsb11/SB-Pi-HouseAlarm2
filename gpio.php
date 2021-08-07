6c017b28486bbed5b8336fbdbac8d6a852a5f368
Stephen Brown <stephen.brown@bourne-grammar.lincs.sch.uk>
2021-08-07 17:35:26 +0100

    Update gpio.php

--- gpio.php
+++ gpio.php
@@ -7,19 +7,31 @@ if (isset ( $_GET["room"] )) {
 
     //test if value is a number
     if ( (is_numeric($pic)) && ($pic <= 16) && ($pic >= 1) ) {
-        //set the gpio's mode to input
+        // Set the GPIO mode to input...
         system("gpio mode ".$pic." in");
 
         // Read the pin's status
         exec ("gpio read ".$pic, $status, $return );
 
-        //set the gpio to high/low
-        // if ($status[0] == "0" ) { $status[0] = "1"; }
-        // else if ($status[0] == "1" ) { $status[0] = "0"; }
-
         // IN CASE NEEDED IN FUTURE: system("gpio write " . $pin . " " . $status );
 
-        // echo the result to the requesting page.
+        /* NOT WORKING. PERMISSIONS MAYBE?
+        // Update log file, then echo the result to the requesting page.
+        if ($status[0] == "0") {
+           if ($pic == "1") { $where="kitchen"; }
+           else if ($pic == "4") { $where = "lounge"; }
+           else if ($pic == "15") { $where = "hall"; }
+           else if ($pic == "16") { $where = "landing"; }
+           else { $where = "NONE"; }
+
+           if ($where != "NONE") {
+              $myfile = fopen($where . ".txt", "w");
+              fwrite($myfile, date("d/m/Y @ h:i:sa"));
+              fclose($myfile);
+           }
+        }
+        */
+
         echo($status[0]);
     } else { echo ("fail"); }
 } else { echo ("fail"); }
